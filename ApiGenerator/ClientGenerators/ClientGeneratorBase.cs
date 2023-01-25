f06f97cabc32c78a93290536745efb691b2cb30b
Niklas Strasser <niklasstrasser@hotmail.com>
2023-01-25 20:52:17 +0100

    use base class instead of interface

---
+++ ApiGenerator/ClientGenerators/ClientGeneratorBase.cs
@@ -0,0 +1,24 @@
+﻿using ApiGenerator.Models;
+using System.Collections.Generic;
+
+namespace ApiGenerator.ClientGenerators;
+
+public abstract class ClientGeneratorBase
+{
+	public ClientGeneratorBase(string projectName)
+	{
+        ProjectName = projectName;
+        this.CodeStringBuilder = new CodeStringBuilder();
+    }
+
+    public CodeStringBuilder CodeStringBuilder { get; }
+
+    public string ProjectName { get; }
+
+    public abstract void GenerateClient(IEnumerable<ControllerClientDetails> controllerClientDetails);
+
+    public virtual void GenerateClient(ControllerClientDetails controllerClientDetails)
+    {
+        this.GenerateClient(new List<ControllerClientDetails> { controllerClientDetails });
+    }
+}
