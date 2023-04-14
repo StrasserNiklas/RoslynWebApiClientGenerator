a07d916ebf40571e29d9673be158b68a81795353
Niklas Strasser <niklasstrasser@hotmail.com>
2023-04-14 23:34:51 +0200

    fixed nswag generation problem LOL

--- ApiClientGenerator.Testing/ComplexTestingApi/Controllers/FromQueryController.cs
+++ ApiClientGenerator.Testing/ComplexTestingApi/Controllers/FromQueryController.cs
@@ -9,41 +9,48 @@ namespace ComplexTestingApi.Controllers;
 public class FromQueryController : ControllerBase
 {
     [HttpGet]
-    public ActionResult Class_Parameter_WithoutAttribute_AllPropertiesAttributed(AllPropertiesQueryAttributed allPropertiesAttributed)
+    public ActionResult ClassParameterWithoutAttributeAllPropertiesAttributed(AllPropertiesQueryAttributed allPropertiesAttributed)
     {
         return Ok();
     }
+
     // check if you need builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressInferBindingSourcesForParameters = true);
     [HttpGet]
     public ActionResult ClassParameterWithoutAttributeSomePropertiesAttributed(SomePropertiesQueryAttributed somePropertiesAttributed)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult ClassParameterWithAttributeSomePropertiesAttributed([FromQuery] SomePropertiesQueryAttributed somePropertiesAttributed)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult ClassParameterWithAttributeAllPropertiesAttributed([FromQuery] AllPropertiesQueryAttributed somePropertiesAttributed)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult ClassParameterWithAttribute([FromQuery] NoPropertiesAttributedClass simpleQueryClass)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult PrimitiveParameterWithAttribute([FromQuery] string soloString)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult PrimitiveParameterNoAttribute(string soloString)
     {
         return Ok();
     }
+
     [HttpGet]
     public ActionResult ClassParameterNoAttribute(NoPropertiesAttributedClass soloString)
     {
